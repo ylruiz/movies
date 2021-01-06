b65fa867349eb2b9ab8752c7920ddba35aa7eea9
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2021-01-06 09:13:38 +0100

    Adding search functionality

--- lib/movie_list.dart
+++ lib/movie_list.dart
@@ -15,6 +15,8 @@ class _MovieListState extends State<MovieList> {
   final String iconBase = 'https://image.tmdb.org/t/p/w92/';
   final String defaultImage =
       'https://images.freeimages.com/images/large-previews/5eb/movie-clapboard-1184339.jpg';
+  Icon visibleIcon = Icon(Icons.search);
+  Widget searchBar = Text('Movies');
 
   @override
   void initState() {
@@ -38,7 +40,34 @@ class _MovieListState extends State<MovieList> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Movies'),
+        title: searchBar,
+        actions: <Widget>[
+          IconButton(
+            icon: visibleIcon,
+            onPressed: () {
+              setState(() {
+                if (this.visibleIcon.icon == Icons.search) {
+                  this.visibleIcon = Icon(Icons.cancel);
+                  this.searchBar = TextField(
+                    textInputAction: TextInputAction.search,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                    onSubmitted: (String text) {
+                      search(text);
+                    },
+                  );
+                } else {
+                  setState(() {
+                    this.visibleIcon = Icon(Icons.search);
+                    this.searchBar = Text('Movies');
+                  });
+                }
+              });
+            },
+          ),
+        ],
       ),
       body: ListView.builder(
         itemCount: (this.moviesCount == null) ? 0 : this.moviesCount,
@@ -59,8 +88,7 @@ class _MovieListState extends State<MovieList> {
                   'Released: ${movies[position].releaseDate} - Vote: ${movies[position].voteAverage.toString()}'),
               onTap: () {
                 MaterialPageRoute route = MaterialPageRoute(
-                  builder: (_) => MovieDetail(movies[position])
-                );
+                    builder: (_) => MovieDetail(movies[position]));
                 Navigator.push(context, route);
               },
             ),
@@ -68,5 +96,13 @@ class _MovieListState extends State<MovieList> {
         },
       ),
     );
+  }
+
+  Future search(text) async {
+    movies = await helper.findMovies(text);
+    setState(() {
+      moviesCount = movies.length;
+      movies = movies;
+    });
   }
 }
