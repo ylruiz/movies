a597b9fd4bb76f12d0b8fd6bea5c3a27a19bf139
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2021-01-06 06:23:01 +0100

    Get a list of movies

--- lib/http.helper.dart
+++ lib/http.helper.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
+
+import 'movie.dart';
 
 class HttpHelper {
   final String urlKey = DotEnv().env['URL_KEY'];
@@ -8,13 +11,15 @@ class HttpHelper {
   final String urlUpcoming = DotEnv().env['URL_UPCOMING'];
   final String urlLanguage = DotEnv().env['URL_LANG'];
 
-  Future<String> getUpcoming() async {
+  Future<List> getUpcoming() async {
     final String upcomming = urlBase + urlUpcoming + urlKey + urlLanguage;
     http.Response result = await http.get(upcomming);
 
     if (result.statusCode == HttpStatus.ok) {
-      String responseBody = result.body;
-      return responseBody;
+      final jsonResponse = json.decode(result.body);
+      final moviesMap = jsonResponse['results'];
+      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
+      return movies;
     } else {
       return null;
     }
