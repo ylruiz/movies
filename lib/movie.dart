3b7329fd5c597ce9e5b8d39ec53aef3fcb11d2c1
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2021-01-06 07:57:37 +0100

    Show the list of movies

--- lib/movie.dart
+++ lib/movie.dart
@@ -12,7 +12,7 @@ class Movie {
   Movie.fromJson(Map<String, dynamic> parsedJson) {
     this.id = parsedJson['id'];
     this.title = parsedJson['title'];
-    this.voteAverage = parsedJson['vote_average'];
+    this.voteAverage = parsedJson['vote_average'] * 1.0;
     this.releaseDate = parsedJson['release_date'];
     this.overview = parsedJson['overview'];
     this.posterPath = parsedJson['poster_path'];
--- lib/movie_list.dart
+++ lib/movie_list.dart
@@ -8,26 +8,45 @@ class MovieList extends StatefulWidget {
 }
 
 class _MovieListState extends State<MovieList> {
-  String result;
   HttpHelper helper;
+  int moviesCount;
+  List movies;
 
   @override
   void initState() {
     helper = HttpHelper();
+    initialize();
     super.initState();
+  }
+
+  Future initialize() async {
+    movies = [];
+    movies = await helper.getUpcoming();
+    setState(() {
+      moviesCount = movies.length;
+      movies = movies;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    helper.getUpcoming().then((value) => setState(() {
-          result = value;
-        }));
     return Scaffold(
       appBar: AppBar(
         title: Text('Movies'),
       ),
-      body: Container(
-        child: Text(result),
+      body: ListView.builder(
+        itemCount: (this.moviesCount == null) ? 0 : this.moviesCount,
+        itemBuilder: (BuildContext context, int position) {
+          return Card(
+            color: Colors.white,
+            elevation: 2,
+            child: ListTile(
+              title: Text(movies[position].title),
+              subtitle: Text(
+                  'Released: ${movies[position].releaseDate} - Vote: ${movies[position].voteAverage.toString()}'),
+            ),
+          );
+        },
       ),
     );
   }
